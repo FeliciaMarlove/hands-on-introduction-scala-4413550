87fcf811532de8e535c85b52655c7368279d5d00
Florence Mary <57141210+FeliciaMarlove@users.noreply.github.com>
2024-09-02 08:40:22 +0000

    hands on and notes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,1 +1,18 @@
-@main def sayHello: Unit = println("Hello World!")
+@main def sayHello: Unit = sayHi("Florence")
+
+// Scala2:
+//def sayHi(name: String): Unit = 
+//  println(s"Hi $name")
+//end sayHi
+
+// New in Scala3:
+//def sayHi(name: String): Unit = {
+//  println(s"Hi $name")
+//}
+
+// Or Scala3:
+def sayHi(name: String): Unit =
+  val listN = List(1, 2, 3)
+  val listS = List("toto", "tata")
+  val listM: List[String | Int | Double] = List(3.0, 1, "hello")
+  println(s"Hi $name")
